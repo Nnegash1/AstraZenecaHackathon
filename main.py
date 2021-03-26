ab3e677b37146789f54e49dd63605edc986ba165
Nahom <Nnegash1@umbc.edu>
2021-03-26 11:35:22 -0400

    layout of the home page

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-def print():
-    print("Hello world")
+from flask import Flask,request, Response
+import json
+
+app = Flask(__name__)
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
+@app.route('/hello')
+def homepage():
+    response = {"message": "Hello World"}
+    return 'Welcome to my page'
+
+if __name__ == '__main__':
+    FLASK_DEBUG = True
+    app.run()
